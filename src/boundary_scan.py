282e82a08dd40e7b9be585650f02448aae6c12e8
Mirko Scholz <srtlg@users.noreply.github.com>
2018-03-20 15:18:25 +0100

    also interpret Altera's style of defining the logical port map

--- src/boundary_scan.py
+++ src/boundary_scan.py
@@ -24,18 +24,34 @@ class Chain(object):
 class Device(object):
     def __init__(self, bsd_ast):
         assert len(bsd_ast.device_package_pin_mappings) == 1
-        assert all([len(d.identifier_list) == 1 for d in bsd_ast.logical_port_description])
         self._name_to_type = dict([(d.identifier_list[0], d.pin_type) for d in bsd_ast.logical_port_description])
         self._device_package = bsd_ast.device_package_pin_mappings[0].pin_mapping_name.replace('_PACKAGE', '')
         self.component_name = bsd_ast.component_name
         self._name_to_pin = None  # type: dict
         self._pin_to_name = None  # type: dict
+        self._name_to_type = self._build_name_to_type(bsd_ast)
         self._build_dicts(bsd_ast)
+
+    @staticmethod
+    def _build_name_to_type(bsd_ast):
+        ret = dict()
+        for description in bsd_ast.logical_port_description:
+            for i in description.identifier_list:
+                ret[i] = description.pin_type
+        return ret
+
+    @staticmethod
+    def _build_name_to_dimension(bsd_ast):
+        ret = dict()
+        for description in bsd_ast.logical_port_description:
+            for i in description.identifier_list:
+                ret[i] = description.port_dimension
+        return ret
 
     def _build_dicts(self, bsd_ast):
         self._name_to_pin = dict()
         self._pin_to_name = dict()
-        name_to_dimension = dict([(d.identifier_list[0], d.port_dimension) for d in bsd_ast.logical_port_description])
+        name_to_dimension = self._build_name_to_dimension(bsd_ast)
         pin_map = PinMapString(bsd_ast.device_package_pin_mappings[0].pin_map)
         for name in list(self._name_to_type.keys()):
             dimension = name_to_dimension[name]
