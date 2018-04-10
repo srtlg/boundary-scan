256b3601d2c7f34acf8237b3af775bb4b9c25b30
Mirko Scholz <srtlg@users.noreply.github.com>
2018-04-10 09:41:27 +0200

    started simple mapping

---
+++ src/mecrisp_pod.py
@@ -0,0 +1,6 @@
+from boundary_scan import TestPOD
+
+
+class MecrispPOD(TestPOD):
+    def __init__(self, port):
+        super().__init__()
---
+++ src/test_simple_mapping.py
@@ -0,0 +1,7 @@
+import unittest
+
+test1 = """\
+STM32F1,net name,XC9572XL
+6,CPLD_CLK_2MHz,43
+7,CPLD_CLK1_410Hz,41
+"""
